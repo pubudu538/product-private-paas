301663be46ce142f3d390e5e77249df7d771c7b7
Pubudu Gunatilaka <pubudu538@gmail.com>
2015-08-20 10:43:58 +0530

    Adding am changes

--- cartridges/docker/wso2am-1.9.0/plugins/wso2am-startup-handler.py
+++ cartridges/docker/wso2am-1.9.0/plugins/wso2am-startup-handler.py
@@ -18,7 +18,7 @@
 
 from plugins.contracts import ICartridgeAgentPlugin
 from modules.util.log import LogFactory
-import entity
+from entity import *
 import subprocess
 import os
 import mdsclient
@@ -38,24 +38,21 @@ class WSO2AMStartupHandler(ICartridgeAgentPlugin):
     CONST_PORT_MAPPINGS = "PORT_MAPPINGS"
     CONST_APPLICATION_ID = "APPLICATION_ID"
     CONST_MB_IP = "MB_IP"
+    CONST_CLUSTER_ID = "CLUSTER_ID"
     CONST_SERVICE_NAME = "SERVICE_NAME"
-    CONST_ESB_WORKER = "esbworker"
     CONST_STRATOS_MEMBERSHIP_SCHEME = "stratos"
     CONST_KEYMANAGER = "KeyManager"
-    CONST_GATEWAY = "Gateway"
+    CONST_GATEWAY_MANAGER = "Gateway-Manager"
+    CONST_GATEWAY_WORKER = "Gateway-Worker"
     CONST_PUBLISHER = "Publisher"
     CONST_STORE = "Store"
-    CONST_KUB_NODE_PORT = "NodePort"
-    CONST_KUB_CLUSTER_IP = "ClusterIP"
-    # CONST_SERVICE_KEYMANAGER = "keymanager"
-    # CONST_SERVICE_GATEWAY = "gateway"
-    # CONST_SERVICE_PUBLISHER = "publisher"
-    # CONST_SERVICE_STORE = "store"
-    TOPOLOGY_KUBERNETES_SERVICES = "kubernetesServices"
-    TOPOLOGY_KUBERNETES_SERVICE_PORTNAME = "portName"
-    TOPOLOGY_KUBERNETES_SERVICE_PORTAL_IP = "portalIP"
+    CONST_PPAAS_MEMBERSHIP_SCHEME = "private-paas"
+    CONST_WORKER = "worker"
+    CONST_MANAGER = "manager"
+    CONST_MGT = "mgt"
 
-    SERVICES = ["esbworker", "esbmanager", "esb"]
+    GATEWAY_SERVICES = ["wso2am-190-gw-manager", "wso2am-190-gw-worker"]
+    PUB_STORE_SERVICES = ["wso2am-190-pub", "wso2am-190-store"]
 
     # list of environment variables exported by the plugin
     ENV_CONFIG_PARAM_MB_HOST = 'CONFIG_PARAM_MB_HOST'
@@ -73,7 +70,8 @@ class WSO2AMStartupHandler(ICartridgeAgentPlugin):
     ENV_CONFIG_PARAM_STORE_IP = 'CONFIG_PARAM_STORE_IP'
     ENV_CONFIG_PARAM_THRIFTSERVERHOST = 'CONFIG_PARAM_THRIFTSERVERHOST'
     ENV_CONFIG_PARAM_WKA_MEMBERS = 'CONFIG_PARAM_WKA_MEMBERS'
-
+    ENV_CONFIG_PARAM_SUB_DOMAIN = 'CONFIG_PARAM_SUB_DOMAIN'
+    ENV_CONFIG_PARAM_WORKER_HOST_NAME = 'CONFIG_PARAM_WORKER_HOST_NAME'
 
     def run_plugin(self, values):
 
@@ -84,6 +82,10 @@ class WSO2AMStartupHandler(ICartridgeAgentPlugin):
         service_name = values[self.CONST_SERVICE_NAME]
         profile = os.environ.get(self.ENV_CONFIG_PARAM_PROFILE)
         load_balancer_ip = os.environ.get(self.ENV_CONFIG_PARAM_LOADBALANCER_IP)
+        membership_scheme = values.get(self.ENV_CONFIG_PARAM_MEMBERSHIP_SCHEME)
+        clustering = values.get(self.ENV_CONFIG_PARAM_CLUSTERING, 'false')
+        my_cluster_id = values[self.CONST_CLUSTER_ID]
+        worker_hostname = os.environ.get(self.ENV_CONFIG_PARAM_WORKER_HOST_NAME)
 
         # log above values
         WSO2AMStartupHandler.log.info("Port Mappings: %s" % port_mappings_str)
@@ -91,9 +93,41 @@ class WSO2AMStartupHandler(ICartridgeAgentPlugin):
         WSO2AMStartupHandler.log.info("MB IP: %s" % mb_ip)
         WSO2AMStartupHandler.log.info("Service Name: %s" % service_name)
         WSO2AMStartupHandler.log.info("Profile: %s" % profile)
+        WSO2AMStartupHandler.log.info("Membership Scheme: %s" % membership_scheme)
+        WSO2AMStartupHandler.log.info("Cluster ID: %s" % my_cluster_id)
+        WSO2AMStartupHandler.log.info("Clustering: %s" % clustering)
+        WSO2AMStartupHandler.log.info("Worker Hostname: %s" % worker_hostname)
+
+        # set sub-domain
+        sub_domain = None
+        if service_name.endswith(self.CONST_MANAGER):
+            sub_domain = self.CONST_MGT
+        elif service_name.endswith(self.CONST_WORKER):
+            sub_domain = self.CONST_WORKER
+        if sub_domain is not None:
+            self.export_env_var(self.ENV_CONFIG_PARAM_SUB_DOMAIN, sub_domain)
 
         # export Proxy Ports as Env. variables - used in catalina-server.xml
-        # self.set_proxy_ports(port_mappings_str)
+        self.set_proxy_ports(port_mappings_str)
+
+        # if CONFIG_PARAM_MEMBERSHIP_SCHEME is not set, set the private-paas membership scheme as default one
+        if membership_scheme is None:
+            membership_scheme = self.CONST_PPAAS_MEMBERSHIP_SCHEME
+            self.export_env_var(self.ENV_CONFIG_PARAM_MEMBERSHIP_SCHEME, membership_scheme)
+
+        if clustering == 'true' and membership_scheme == self.CONST_PPAAS_MEMBERSHIP_SCHEME:
+            # export Cluster_Ids as Env. variables - used in axis2.xml
+            service_list = None
+
+            if service_name in self.GATEWAY_SERVICES:
+                service_list = self.GATEWAY_SERVICES
+            elif service_name in self.PUB_STORE_SERVICES:
+                service_list = self.PUB_STORE_SERVICES
+
+            self.set_cluster_ids(app_id, service_name, my_cluster_id, service_list)
+            # export mb_ip as Env.variable - used in jndi.properties
+            if mb_ip is not None:
+                self.export_env_var(self.ENV_CONFIG_PARAM_MB_HOST, mb_ip)
 
         if profile == self.CONST_KEYMANAGER:
 
@@ -104,16 +138,34 @@ class WSO2AMStartupHandler(ICartridgeAgentPlugin):
             self.export_env_var(self.ENV_CONFIG_PARAM_GATEWAY_IP, gateway_ip)
             start_command = "exec ${CARBON_HOME}/bin/wso2server.sh -Dprofile=api-key-manager start"
 
-        elif profile == self.CONST_GATEWAY:
+        elif profile == self.CONST_GATEWAY_MANAGER:
 
             isKubernetes = self.check_for_kubernetes_cluster(service_name, app_id)
             service_ip = self.configure_service_ip(load_balancer_ip, isKubernetes, service_name, app_id)
             keymanager_ip = self.configure_profile(app_id, self.ENV_CONFIG_PARAM_GATEWAY_IP, service_ip,
                                                    self.ENV_CONFIG_PARAM_KEYMANAGER_IP)
             self.export_env_var(self.ENV_CONFIG_PARAM_KEYMANAGER_IP, keymanager_ip)
-            self.export_env_var(self.ENV_CONFIG_PARAM_THRIFTSERVERHOST, keymanager_ip)
+
+            member_ip = socket.gethostbyname(socket.gethostname())
+            self.update_hosts_file(member_ip, worker_hostname)
+
+            #  self.export_env_var(self.ENV_CONFIG_PARAM_THRIFTSERVERHOST, keymanager_ip)
             start_command = "exec ${CARBON_HOME}/bin/wso2server.sh -Dprofile=gateway-manager start"
 
+        elif profile == self.CONST_GATEWAY_WORKER:
+
+            # isKubernetes = self.check_for_kubernetes_cluster(service_name, app_id)
+            # service_ip = self.configure_service_ip(load_balancer_ip, isKubernetes, service_name, app_id)
+            # keymanager_ip = self.configure_profile(app_id, self.ENV_CONFIG_PARAM_GATEWAY_IP, service_ip,
+            #                                       self.ENV_CONFIG_PARAM_KEYMANAGER_IP)
+            keymanager_ip = self.get_data_from_meta_data_service(app_id, self.ENV_CONFIG_PARAM_KEYMANAGER_IP)
+            self.export_env_var(self.ENV_CONFIG_PARAM_KEYMANAGER_IP, keymanager_ip)
+            # self.export_env_var(self.ENV_CONFIG_PARAM_THRIFTSERVERHOST, keymanager_ip)
+
+            member_ip = socket.gethostbyname(socket.gethostname())
+            self.update_hosts_file(member_ip, worker_hostname)
+
+            start_command = "exec ${CARBON_HOME}/bin/wso2server.sh -Dprofile=gateway-worker start"
 
         elif profile == self.CONST_PUBLISHER:
 
@@ -146,11 +198,10 @@ class WSO2AMStartupHandler(ICartridgeAgentPlugin):
             self.export_env_var(self.ENV_CONFIG_PARAM_KEYMANAGER_IP, keymanager_ip)
             self.export_env_var(self.ENV_CONFIG_PARAM_GATEWAY_IP, gateway_ip)
             start_command = "exec ${CARBON_HOME}/bin/wso2server.sh -Dprofile=api-store start"
+
         else:
-            start_command = "exec ${CARBON_HOME}/bin/wso2server.sh start"
+            start_command = "exec ${CARBON_HOME}/bin/wso2server.sh start"  # start configurator
 
-
-        # start configurator
         WSO2AMStartupHandler.log.info("Configuring WSO2 API Manager...")
         config_command = "python ${CONFIGURATOR_HOME}/configurator.py"
         env_var = os.environ.copy()
@@ -167,7 +218,6 @@ class WSO2AMStartupHandler(ICartridgeAgentPlugin):
 
 
     def configure_service_ip(self, service_ip, isKubernetes, service_name, app_id):
-
         if service_ip is None:
             if isKubernetes:
                 service_ip = self.read_portal_ip_of_service(service_name, app_id)
@@ -180,7 +230,6 @@ class WSO2AMStartupHandler(ICartridgeAgentPlugin):
 
 
     def configure_profile(self, app_id, publish_property, value, receive_property):
-
         self.remove_data_from_metadata(publish_property)
         self.add_values_to_meta_data_service(publish_property, value)
 
@@ -188,7 +237,6 @@ class WSO2AMStartupHandler(ICartridgeAgentPlugin):
 
 
     def get_data_from_meta_data_service(self, app_id, receive_data):
-
         mds_response = None
         while mds_response is None:
             WSO2AMStartupHandler.log.info(
@@ -202,14 +250,12 @@ class WSO2AMStartupHandler(ICartridgeAgentPlugin):
 
 
     def add_values_to_meta_data_service(self, key, value):
-
         mdsclient.MDSPutRequest()
         data = {"key": key, "values": [value]}
         mdsclient.put(data, app=True)
 
 
     def remove_data_from_metadata(self, key):
-
         mds_response = mdsclient.get(app=True)
 
         if mds_response is not None and mds_response.properties.get(key) is not None:
@@ -222,10 +268,16 @@ class WSO2AMStartupHandler(ICartridgeAgentPlugin):
                 for entry in read_data:
                     mdsclient.delete_property_value(key, entry)
 
-    def set_cluster_ids(self, app_id):
+
+    def set_cluster_ids(self, app_id, service_type, my_cluster_id, service_list):
+        """
+        Set clusterIds of services read from topology for worker manager instances
+        else use own clusterId
+        :return: void
+        """
         cluster_ids = []
 
-        for service_name in self.SERVICES:
+        for service_name in service_list:
             cluster_id_of_service = self.read_cluster_id_of_service(service_name, app_id)
             if cluster_id_of_service is not None:
                 cluster_ids.append(cluster_id_of_service)
@@ -235,9 +287,10 @@ class WSO2AMStartupHandler(ICartridgeAgentPlugin):
             cluster_ids_string = ",".join(cluster_ids)
             self.export_env_var(self.ENV_CONFIG_PARAM_CLUSTER_IDs, cluster_ids_string)
 
+
     def read_member_ip_from_topology(self, service_name, app_id):
         clusters = None
-        topology = entity.TopologyContext().get_topology()
+        topology = TopologyContext().get_topology()
         members = None
         member_ip = None
 
@@ -261,6 +314,7 @@ class WSO2AMStartupHandler(ICartridgeAgentPlugin):
             member_ip = socket.gethostbyname(server_hostname)
 
         return member_ip
+
 
     # exports proxy ports as env. variables
     def set_proxy_ports(self, port_mappings_str):
@@ -299,6 +353,7 @@ class WSO2AMStartupHandler(ICartridgeAgentPlugin):
         self.export_env_var(self.ENV_CONFIG_PARAM_PT_HTTP_PROXY_PORT, pt_http_port)
         self.export_env_var(self.ENV_CONFIG_PARAM_PT_HTTPS_PROXY_PORT, pt_https_port)
 
+
     def export_env_var(self, variable, value):
         if value is not None:
             os.environ[variable] = value
@@ -306,10 +361,10 @@ class WSO2AMStartupHandler(ICartridgeAgentPlugin):
         else:
             WSO2AMStartupHandler.log.warn("Could not export environment variable %s " % variable)
 
-    def read_portal_ip_of_service(self, service_name, app_id):
 
+    def read_portal_ip_of_service(self, service_name, app_id):
         clusters = None
-        topology = entity.TopologyContext().get_topology()
+        topology = TopologyContext().get_topology()
         portalIp_value = None
 
         if topology.service_exists(service_name):
@@ -334,10 +389,9 @@ class WSO2AMStartupHandler(ICartridgeAgentPlugin):
 
 
     def check_for_kubernetes_cluster(self, service_name, app_id):
-
         isKubernetes = False
         clusters = None
-        topology = entity.TopologyContext().get_topology()
+        topology = TopologyContext().get_topology()
 
         if topology.service_exists(service_name):
             service = topology.get_service(service_name)
@@ -351,3 +405,36 @@ class WSO2AMStartupHandler(ICartridgeAgentPlugin):
                     isKubernetes = cluster.is_kubernetes_cluster
 
         return isKubernetes
+
+
+    def read_cluster_id_of_service(self, service_name, app_id):
+        cluster_id = None
+        clusters = None
+        topology = TopologyContext.topology
+
+        if topology is not None:
+            if topology.service_exists(service_name):
+                service = topology.get_service(service_name)
+                clusters = service.get_clusters()
+            else:
+                WSO2AMStartupHandler.log.warn("[Service] %s is not available in topology" % service_name)
+
+            if clusters is not None:
+                for cluster in clusters:
+                    if cluster.app_id == app_id:
+                        cluster_id = cluster.cluster_id
+
+        return cluster_id
+
+
+    def update_hosts_file(self, ip_address, host_name):
+        """
+        Updates /etc/hosts file with clustering hostnames
+        :return: void
+        """
+        config_command = "echo %s  %s >> /etc/hosts" % (ip_address, host_name)
+        env_var = os.environ.copy()
+        p = subprocess.Popen(config_command, env=env_var, shell=True)
+        output, errors = p.communicate()
+        WSO2AMStartupHandler.log.info(
+            "Successfully updated [ip_address] %s & [hostname] %s in etc/hosts" % (ip_address, host_name))
